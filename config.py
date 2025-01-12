83ed464471cdd627bb3913c91578c5e7169f8045
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-12 19:48:42 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+# MongoDB connection string (you can use your provided URI here)
+MONGO_URI = "mongodb+srv://hamzahussain14hh:hamza1208@cluster0.as2ir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = "flask_mongo_crud_db"
