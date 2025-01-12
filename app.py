4b8cf5954a40d586183fbd55bb4bf45e38cf3adc
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-12 19:53:57 +0500

    deployment file for vercel

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-# Import the Flask class from the flask module
 from flask import Flask
-from Mongo import item_routes
-# Create an instance of the Flask class.
-# The __name__ variable is used to determine the root path of the application.
+from Mongo  import item_routes
+
 app = Flask(__name__)
 
-# Define a route for the root URL ('/').
-# When a user visits 'http://127.0.0.1:8000/', this function will be triggered.
-@app.route('/')
-def hell0():
-    # Return a simple string as the response.
-    return 'I am running on flask'
-
+# Register the Blueprint for item routes
 app.register_blueprint(item_routes)
+
+# Define the home route for basic testing
+@app.route('/')
+def home():
+    return "Flask app deployed on Vercel!", 200
+
 
 # This block ensures the application runs only when executed directly.
 # It prevents the code from running if the file is imported as a module elsewhere.
@@ -21,3 +19,4 @@ if __name__ == '__main__':
     # - `debug=True` enables debug mode, which provides detailed error messages and auto-reloads the server when changes are made to the code.
     # - `port=8000` specifies the port the server should run on (default is 5000 if not specified).
     app.run(debug=True, port=8000)
+
